1a71609803af4ca37467ae5eccd89e8394dd9f5a
BobbyGaDouche <info@brewed.online>
2023-06-05 12:57:41 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}...`));
+
+const Shopify = require('shopify-api-node');
+
+const shopify = new Shopify({
+  shopName: 'brewedonline',
+  apiKey: 'f407beb8cca59260ce936842917934cd',
+  password: 'c537296e3cc78220350920f12d7c23d7',
+});
+
+
+app.get('/tag-customer/:id', async (req, res) => {
+  try {
+    const customerId = req.params.id;
+    const customer = await shopify.customer.get(customerId);
+
+    customer.tags += ', new-tag'; // Append new tag
+
+    await shopify.customer.update(customerId, customer);
+
+    res.status(200).send('Successfully tagged customer');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('An error occurred while tagging the customer');
+  }
+});

6a77c582fee6db87862b3ff35096ddcff919639d
BobbyGaDouche <info@brewed.online>
2023-06-05 14:02:14 +0100

    Your commit message here

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const Shopify = require('shopify-api-node');
+const shopifyApiKey = process.env.SHOPIFY_API_KEY;
+const shopifyApiSecret = process.env.SHOPIFY_API_SECRET;
+
+console.log('Shopify API Key:', shopifyApiKey);
+console.log('Shopify API Secret:', shopifyApiSecret);
+
+if (!shopifyApiKey || !shopifyApiSecret) {
+  console.error('Missing Shopify API Key or API Secret');
+  process.exit(1);
+}
+
 const shopify = new Shopify({
   shopName: 'brewedonline',
-  apiKey: process.env.SHOPIFY_API_KEY,
-  password: process.env.SHOPIFY_API_SECRET,
+  apiKey: shopifyApiKey,
+  password: shopifyApiSecret,
 });
-
-// Log the values of the API keys
-console.log('API Key:', process.env.SHOPIFY_API_KEY);
-console.log('API Secret:', process.env.SHOPIFY_API_SECRET);
 
 app.get('/tag-customer/:id', async (req, res) => {
   try {

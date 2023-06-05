a34edabc391dd260eb91ac47d2e282040a40319c
BobbyGaDouche <info@brewed.online>
2023-06-05 14:32:20 +0100

    Your commit message here

--- app.js
+++ app.js
@@ -16,44 +16,23 @@ if (!shopifyApiKey || !shopifyApiSecret) {
 
 const shopify = new Shopify({
   shopName: 'brewedonline',
-  apiKey: process.env.SHOPIFY_API_KEY,
-  password: process.env.SHOPIFY_API_SECRET,
+  apiKey: shopifyApiKey,
+  password: shopifyApiSecret,
 });
 
-
-app.put('/admin/api/2023-01/customers/:id.json', async (req, res) => {
+// Update a customer
+app.put('/update-customer/:id', async (req, res) => {
   try {
     const customerId = req.params.id;
-    const customer = await shopify.customer.get(customerId);
+    const updatedCustomerData = req.body; // Contains the updated customer information
 
-    // Update the customer's metafield
-    const metafield = {
-      key: 'new',
-      value: 'newvalue',
-      type: 'single_line_text_field',
-      namespace: 'global'
-    };
+    // Perform the necessary operations to update the customer using the Shopify API
+    const updatedCustomer = await shopify.customer.update(customerId, updatedCustomerData);
 
-    // Check if the metafield already exists for the customer
-    const existingMetafield = customer.metafields.find(mf => mf.key === metafield.key);
-    if (existingMetafield) {
-      existingMetafield.value = metafield.value; // Update the existing metafield value
-    } else {
-      customer.metafields.push(metafield); // Add the new metafield
-    }
-
-    await shopify.customer.update(customerId, customer);
-
-    res.status(200).send('Successfully updated customer metafield');
+    res.status(200).json(updatedCustomer);
   } catch (error) {
-    console.error('Error:', error);
-    console.error('Error message:', error.message);
-    console.error('Error code:', error.code);
-    console.error('Error status:', error.status);
-    console.error('Error headers:', error.headers);
-    console.error('Error response body:', error.body);
-
-    res.status(500).send('An error occurred while updating the customer metafield');
+    console.error("Error:", error);
+    res.status(500).send('An error occurred while updating the customer');
   }
 });
 

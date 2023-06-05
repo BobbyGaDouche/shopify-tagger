aadcc11205e7d43c42e5894bff3114c43d9441b1
BobbyGaDouche <info@brewed.online>
2023-06-05 16:31:08 +0100

    Your commit message here

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
 const express = require('express');
-const Shopify = require('shopify-api-node');
+const Shopify = require('@shopify/shopify-api');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Use environment variables to keep sensitive information secure
+const store = process.env.SHOPIFY_STORE;
+const apiKey = process.env.SHOPIFY_API_KEY;
+const password = process.env.SHOPIFY_API_SECRET;
+const accessToken = process.env.SHOPIFY_ADMIN_API_TOKEN;
+
 // Set up Shopify API
-const shopify = new Shopify({
-  shopName: 'brewedonline',
-  apiKey: process.env.SHOPIFY_API_KEY,
-  password: process.env.SHOPIFY_API_SECRET,
-});
+const shopify = new Shopify.Clients.Rest(store, apiKey, password);
+
+// Set the Shopify request headers
+shopify.options.headers['X-Shopify-Access-Token'] = accessToken;
 
 app.get('/tag-customer/:id', async (req, res) => {
   try {
     const customerId = req.params.id;
-    const customer = await shopify.customer.get(customerId);
+    const response = await shopify.get(`/admin/api/2023-01/customers/${customerId}.json`);
+    const customer = response.body.customer;
 
     if (typeof customer.tags === 'string' && !customer.tags.includes('new-tag')) {
       customer.tags += ', new-tag';
     }
 
-    await shopify.customer.update(customerId, { tags: customer.tags });
+    const updatedCustomer = {
+      id: customerId,
+      tags: customer.tags,
+    };
+
+    await shopify.put(`/admin/api/2023-01/customers/${customerId}.json`, { customer: updatedCustomer });
 
     res.status(200).send('Successfully tagged customer');
   } catch (error) {

87cd1cde1abeeb8458f64e9565d124420e594fa6
BobbyGaDouche <info@brewed.online>
2023-06-05 13:54:34 +0100

    Your commit message here

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const Shopify = require('shopify-api-node');
 const shopify = new Shopify({
